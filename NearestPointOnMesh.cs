728d5bbb0a68b1348b14146dc93a4abc7b629d55
Arrnav <arnav.m1496@gmail.com>
2020-08-21 14:37:40 +0530

    Added NearestPointOnMesh.cs & SubMeshCombine.cs

---
+++ NearestPointOnMesh.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Returns world position of the vertex on a Gameobject's mesh nearest to Input point
+/// </summary>
+public static class NearestPointOnMesh
+{
+    public static Vector3 NearestVertexTo(Vector3 point, GameObject Object)
+    {
+        Transform t = Object.transform;
+
+        // convert point to local space
+        point = t.InverseTransformPoint(point);
+        Mesh mesh = Object.GetComponent<MeshFilter>().mesh;
+
+        float minDistanceSqr = Mathf.Infinity;
+        Vector3 nearestVertex = Vector3.zero;
+        // scan all vertices to find nearest
+        foreach (Vector3 vertex in mesh.vertices)
+        {
+            Vector3 diff = point - vertex;
+            float distSqr = diff.sqrMagnitude;
+            if (distSqr < minDistanceSqr)
+            {
+                minDistanceSqr = distSqr;
+                nearestVertex = vertex;
+            }
+        }
+        // convert nearest vertex back to world space
+        return t.TransformPoint(nearestVertex);
+    }
+}
---
+++ SubMeshCombine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class SubMeshCombine
+{
+    public static Mesh CombineAllToOne(Mesh mesh, Renderer rend)
+    {
+        Mesh m = mesh;
+        m.SetTriangles(mesh.triangles, 0);
+        m.subMeshCount = 1;
+        m.Optimize();
+
+        Material mat = rend.sharedMaterials[0];
+        rend.sharedMaterials = new Material[1];
+        rend.sharedMaterial = mat;
+        return m;
+    }
+}
